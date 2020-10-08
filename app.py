512d7e62997642bb836b82ed458d9cb19accd9fc
Jose Tlacuilo <A01420128@itesm.mx>
2020-10-07 21:07:50 -0500

    init

--- app.py
+++ app.py
@@ -1,96 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
 app = Flask(__name__)
 
-PROCESOS= {
-    "A": {
-        "Duracion": 400,
-        "Bloqueos": 2,
-        "Inicio_ejecucion": 3000 
-    },
-    "B": {
-        "Duracion": 300,
-        "Bloqueos": 2,
-        "Inicio_ejecucion": 0 
-    },
-    "C": {
-        "Duracion": 50,
-        "Bloqueos": 2,
-        "Inicio_ejecucion": 3000 
-    },
-    "D": {
-        "Duracion": 100,
-        "Bloqueos": 2,
-        "Inicio_ejecucion": 0 
-    },
-    "E": {
-        "Duracion": 1000,
-        "Bloqueos": 5,
-        "Inicio_ejecucion": 3000
-    },
-    "F": {
-        "Duracion": 500,
-        "Bloqueos": 3,
-        "Inicio_ejecucion": 0 
-    },
-    "G": {
-        "Duracion": 10,
-        "Bloqueos": 2,
-        "Inicio_ejecucion": 3000 
-    },
-    "H": {
-        "Duracion": 700,
-        "Bloqueos": 4,
-        "Inicio_ejecucion": 0 
-    },
-    "I": {
-        "Duracion": 450,
-        "Bloqueos": 3,
-        "Inicio_ejecucion": 3000 
-    },
-    "J": {
-        "Duracion": 300,
-        "Bloqueos": 2,
-        "Inicio_ejecucion": 1500 
-    },
-    "K": {
-        "Duracion": 100,
-        "Bloqueos": 2,
-        "Inicio_ejecucion": 4000 
-    },
-    "L": {
-        "Duracion": 3000,
-        "Bloqueos": 5,
-        "Inicio_ejecucion": 1500 
-    },
-    "M": {
-        "Duracion": 80,
-        "Bloqueos": 2,
-        "Inicio_ejecucion": 4000 
-    },
-    "N": {
-        "Duracion": 50,
-        "Bloqueos": 2,
-        "Inicio_ejecucion": 1500 
-    },
-    "Ñ": {
-        "Duracion": 500,
-        "Bloqueos": 3,
-        "Inicio_ejecucion": 8000 
-    },
-    "O": {
-        "Duracion": 600,
-        "Bloqueos": 3,
-        "Inicio_ejecucion": 1500 
-    },
-    "P": {
-        "Duracion": 800,
-        "Bloqueos": 4,
-        "Inicio_ejecucion": 4000 
-    }
-}
 
 @app.route("/", methods=['GET', 'POST'])
-def hello():
+def index(name='no'):
+    if request.method == 'POST':
+        numProc = request.form['numProc']
+        tamQuan = request.form['tamQuan']
+        tiemCC = request.form['tiemCC']
+        tiemBloq = request.form['tiemBloq']
+        return redirect(url_for('index', name=tiemCC))
     name = "Ximul Debian"
     return render_template('index.html', name=name)
 
