d5b4335295c62982aeec0d46884c19e6f7d125d7
Jose Tlacuilo <A01420128@itesm.mx>
2020-10-21 19:55:06 -0500

    Fix floating point inputs

--- app.py
+++ app.py
@@ -10,16 +10,19 @@ import utils
 @app.route("/", methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        numProc = int(request.form['numProc'])
-        tamQuan = int(request.form['tamQuan'])
-        tiemCC = int(request.form['tiemCC'])
-        tiemBloq = int(request.form['tiemBloq'])
-        placeholders = { "numProc": numProc, "tamQuan": tamQuan, "tiemCC": tiemCC, "tiemBloq": tiemBloq }
-        if numProc <= 0 or tamQuan <= 0 or tiemCC < 0 or tiemBloq < 0:
-            return render_template('index.html', micros={}, placeholder=placeholders, msg="Ocurrio un error")
-        else:
-            micros = utils.main(tiemCC, tiemBloq, numProc, tamQuan)
-            return render_template('index.html', micros=micros, placeholder=placeholders, msg="")
+        try: 
+            numProc = int(request.form['numProc'])
+            tamQuan = int(request.form['tamQuan'])
+            tiemCC = int(request.form['tiemCC'])
+            tiemBloq = int(request.form['tiemBloq'])
+            placeholders = { "numProc": numProc, "tamQuan": tamQuan, "tiemCC": tiemCC, "tiemBloq": tiemBloq }
+            if numProc <= 0 or tamQuan <= 0 or tiemCC < 0 or tiemBloq < 0:
+                return render_template('index.html', micros={}, placeholder=placeholders, msg="Ocurrio un error")
+            else:
+                micros = utils.main(tiemCC, tiemBloq, numProc, tamQuan)
+                return render_template('index.html', micros=micros, placeholder=placeholders, msg="")
+        except:
+            return render_template('index.html', micros={}, placeholder={}, msg="Ocurrio un error")
     return render_template('index.html', micros={}, placeholder={}, msg="")
 
 if __name__ == "__main__":
